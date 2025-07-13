1990231b6bc16e7a7750a5abab0fc8eeea56a10b
Jon Bell <github@lot23.com>
2025-07-13 21:10:48 +1200

    New

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,16 @@
-import { authMiddleware } from "@clerk/nextjs";
+// Temporarily disabled middleware to debug
+// import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({
-  publicRoutes: ["/", "/sign-in", "/sign-up"],
-  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\\.[\\w]+$)"]
-});
+// export default authMiddleware({
+//   publicRoutes: ["/", "/sign-in", "/sign-up", "/api/(.*)"],
+//   ignoredRoutes: ["/_next/(.*)", "/favicon.ico"]
+// });
 
-export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-};
+// export const config = {
+//   matcher: ['/((?!_next/static|_next/image|favicon.ico).*)', '/'],
+// };
+
+export default function middleware() {
+  // No-op middleware for debugging
+  return;
+}
